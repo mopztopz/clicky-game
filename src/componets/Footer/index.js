fda1dccc728b8a96027d9a131816981e35ccf93a
Mopztopz <stephenbrown83@gmail.com>
2019-02-04 19:41:29 -0600

    adding Click, Footer and Nav comps

---
+++ src/componets/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer className="footer">
+        < div className="bottom">
+         Mopz Topz Clicky Game! <img alt="react" src="#"/>
+         </ div>
+         </footer>
+    );
+}
+
+export default Footer;
---
+++ src/componets/Nav/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Message from "../";
+import { AST_PropAccess } from "terser";
+
+
+function Nav(props){
+    return(
+        <nav className="navbar">
+        <ul>
+            <li className="brand">
+                <a href="/">Mopz Topz Clicky Game!</a>
+            </li>
+            <Message Score= {props.score} topScore={props.topScore} />
+            <li>
+                Score: {props.score} Top Score: {props.topScore}
+            </li>
+        </ul>
+        </nav>
+    );
+}
+
+export default Nav;
